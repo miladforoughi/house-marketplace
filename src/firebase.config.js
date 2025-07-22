bc2669aa95943524c7566928cb34cdb073fd1e97
Milad Foroughi <miladpro2020@gmail.com>
2025-07-22 21:58:02 +0330

    Install Firebase and Config Firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCALkiZdL0WIVSNScdjjUEsPBl2XMipd8I',
+  authDomain: 'house-marketplace-e44bc.firebaseapp.com',
+  projectId: 'house-marketplace-e44bc',
+  storageBucket: 'house-marketplace-e44bc.firebasestorage.app',
+  messagingSenderId: '216097240682',
+  appId: '1:216097240682:web:8e97e8b17e81b7d4023c13',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
